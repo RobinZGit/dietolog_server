2127c2945f4d04553a3b87581b55cb84df15089d
RobinZGit <psl1@mail.ru>
2023-05-26 17:56:57 +0300

    feat: it doesnt matter)

--- src/main/java/com/dietolog_server/spring/jpa/postgresql/controller/ProductsController.java
+++ src/main/java/com/dietolog_server/spring/jpa/postgresql/controller/ProductsController.java
@@ -8,10 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dietolog_server.spring.jpa.postgresql.model.Info;
 import com.dietolog_server.spring.jpa.postgresql.model.Nutrients;
 import com.dietolog_server.spring.jpa.postgresql.model.Products;
-import com.dietolog_server.spring.jpa.postgresql.repository.ProductsRepository;
 import com.dietolog_server.spring.jpa.postgresql.repository.InfoRepository;
 import com.dietolog_server.spring.jpa.postgresql.repository.NutrientsRepository;
+import com.dietolog_server.spring.jpa.postgresql.repository.ProductsRepository;
 
 @CrossOrigin //(origins = "http://localhost:8081")
 @RestController
@@ -166,6 +164,21 @@ public class ProductsController {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
+
+	/*
+	@PostMapping("/norm2")
+	public ResponseEntity<String> createTutorial(@RequestBody String products) {
+		try {
+			String _norm =  products;//infoRepository.findInfo().toString()
+			//		.save(new Products(product.getName(), product.getLowercase()));
+			HttpHeaders headers = new HttpHeaders();
+			return ResponseEntity.ok().headers(headers).body(products);//new ResponseEntity<>(_norm, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	*/
 
 	/*
 	@PostMapping("/products")
